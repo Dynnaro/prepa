c19d8caf8488fbebcf927de16145d47785f0b2ac
MohamedAziz-Khezami <136236077+MohamedAziz-Khezami@users.noreply.github.com>
2024-06-13 20:03:51 +0100

    Update about.py

--- src/pages/about.py
+++ src/pages/about.py
@@ -7,7 +7,7 @@ import plotly.express as px
 
 dash.register_page(__name__, order=2, path="/about")
 
-df = pd.read_csv("/Users/mak/Desktop/Code_With_Me/project/prepa-insights/c.csv")
+df = pd.read_csv("c.csv")
 
 about_us = html.P(['This project is made from students to Students.', html.Br(), 'The goal is to provide Prepa studnets with informations about each engineering school to help them make the appropriate choice for their orientation.'], className="border rounded bg-light p-3")
 
