27002c03019d2a4a3cf9d00a2c45d749b2eab6c3
Mohamed Aziz Khezami <khezamimohamedaziz@tbs.u-tunis.tn>
2024-06-13 20:21:42 +0100

    changes

--- src/pages/about.py
+++ src/pages/about.py
@@ -7,7 +7,7 @@ import plotly.express as px
 
 dash.register_page(__name__, order=2, path="/about")
 
-df = pd.read_csv("prepa/c.csv")
+
 
 about_us = html.P(['This project is made from students to Students.', html.Br(), 'The goal is to provide Prepa studnets with informations about each engineering school to help them make the appropriate choice for their orientation.'], className="border rounded bg-light p-3")
 
@@ -16,9 +16,7 @@ layout = html.Div(
     [
 
         about_us,
-        dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-        dcc.Graph(id='graph-content'),
-        dl.Map(dl.TileLayer(), center=[56,10], zoom=6, style={'height': '70vh'})
+
         
     ],
     style={
@@ -35,10 +33,3 @@ layout = html.Div(
     
 )
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
