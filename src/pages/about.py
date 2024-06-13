c598198f4b7f3202f64b6c776cedac13b19362d5
Mohamed Aziz Khezami <khezamimohamedaziz@tbs.u-tunis.tn>
2024-06-13 20:03:02 +0100

    new

--- src/pages/about.py
+++ src/pages/about.py
@@ -1,11 +1,13 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html, State, callback
-
-
+import dash_leaflet as dl
+import pandas as pd
+import plotly.express as px
 
 dash.register_page(__name__, order=2, path="/about")
 
+df = pd.read_csv("/Users/mak/Desktop/Code_With_Me/project/prepa-insights/c.csv")
 
 about_us = html.P(['This project is made from students to Students.', html.Br(), 'The goal is to provide Prepa studnets with informations about each engineering school to help them make the appropriate choice for their orientation.'], className="border rounded bg-light p-3")
 
@@ -14,6 +16,9 @@ layout = html.Div(
     [
 
         about_us,
+        dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+        dcc.Graph(id='graph-content'),
+        dl.Map(dl.TileLayer(), center=[56,10], zoom=6, style={'height': '70vh'})
         
     ],
     style={
@@ -29,3 +34,11 @@ layout = html.Div(
     
     
 )
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
