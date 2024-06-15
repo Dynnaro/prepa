3f45269695ceeb2cfb2b6a43b9530ebfd1c26f5c
Rayene <66129629+Dynnaro@users.noreply.github.com>
2024-06-15 15:19:07 +0100

    Update about.py

--- src/pages/about.py
+++ src/pages/about.py
@@ -9,7 +9,7 @@ dash.register_page(__name__, order=2, path="/about")
 
 
 
-about_us = html.P(['This project is made from students to Students.', html.Br(), 'The goal is to provide Prepa studnets with informations about each engineering school to help them make the appropriate choice for their orientation.'], className="border rounded bg-light p-3")
+about_us = html.P(['This project is made from students to Students.', html.Br(), 'The goal is to provide Prepa students with informations about each engineering school to help them make the appropriate choice for their orientation.'], className="border rounded bg-light p-3")
 
 
 layout = html.Div(
